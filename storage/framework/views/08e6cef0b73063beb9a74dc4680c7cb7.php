d89a0c07b8e7a3e2bbe846647f3225dd2151edf1
Ivácsony Szilárd <ivacsony.szilard@gmail.com>
2025-02-26 03:30:06 +0200

    Add files via upload

---
+++ storage/framework/views/08e6cef0b73063beb9a74dc4680c7cb7.php
@@ -0,0 +1,16 @@
+<h1>Password Reset</h1>
+
+<p>You are receiving this email because you requested a password reset.</p>
+
+<p>Reset your password by clicking on the button bellow</p>
+
+<br>
+
+<a href=<?php echo e($verificationLink); ?>}}>
+    <button>Reset Password</button>
+</a>
+
+<p>Please use this link to reset your password. This code will expire in 10 minutes.</p>
+
+<p>If you did not request a password reset, no further action is required.</p>
+<?php /**PATH /home/bilo/projects/realEstate/backend/resources/views/emails/password_reset.blade.php ENDPATH**/ ?>
